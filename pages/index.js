4a410a7dc97014d48ca634d9aa84d21c04dabef9
Kevinoh47 <kohok47@gmail.com>
2019-10-10 14:41:25 -0700

    added POST, PUT, PATCH, DELETE. PATCH not working yet

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import superagent from 'superagent';
 
@@ -17,26 +19,41 @@ export default class Index extends React.Component {
     super(props);
     this.state = {
       url: '',
-      method: '',
+      method: 'get',
+      requestBody: '',
       header: {},
       body: {}
     };
 
   }
 
+  changeMethod = (event) => {
+    let method = event.target.value;
+    this.setState({method});
+  };
+
   changeURL = (event) => {
     let url = event.target.value;
     this.setState({url});
   };
 
+  changeRequestBody = (event) => {
+    let requestBody = event.target.value;
+    this.setState({requestBody});
+  };
+
   callAPI = (event) => {
     event.preventDefault();
-    superagent.get(this.state.url)
+    try {
+      let requestBody = this.state.requestBody && JSON.parse(this.state.requestBody);
+      superagent(this.state.method, this.state.url)
+      .send(requestBody)
       .then( response => {
         let body = response.body;
         this.setState({body});
         console.log(this.state);
       })
+    } catch(e) { console.error('Invalid JSON body: ', this.state.requestBody); }
   };
 
 
@@ -45,7 +62,25 @@ export default class Index extends React.Component {
     return (
       <section>
         <form onSubmit={this.callAPI}>
+          <select name="method" onChange={this.changeMethod}>
+            <option value="get">GET</option>
+            <option value="post">POST</option>
+            <option value="put">PUT</option>
+            <option value="patch">PATCH</option>
+            <option value="delete">DELETE</option>
+          </select>
+          
           <input placeholder="URL" onChange={this.changeURL}/>
+
+          <button>Go!</button>
+          <div>
+            Temporary: Copy paste this for now: http://localhost:3000/products or http://localhost:3000/cards
+          </div>
+          <div>
+            <textarea name="requestBody" onChange={this.changeRequestBody}>
+
+            </textarea>
+          </div>
         </form>
         <ReactJson src={this.state.body} />
       </section>
